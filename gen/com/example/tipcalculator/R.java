8be98199b62f7321a22a6dbc313b1af01bc90cbb
Antoine Falquerho <antoine.falquerho@gmail.com>
2014-06-07 16:58:03 -0700

    User can select custom tip percentage if desired

--- gen/com/example/tipcalculator/R.java
+++ gen/com/example/tipcalculator/R.java
@@ -14,11 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int bv10=0x7f060000;
-        public static final int bv15=0x7f060001;
-        public static final int bv20=0x7f060002;
-        public static final int etAmount=0x7f060004;
-        public static final int tvTip=0x7f060003;
+        public static final int bv10=0x7f060003;
+        public static final int bv15=0x7f060002;
+        public static final int bv20=0x7f060004;
+        public static final int etAmount=0x7f060001;
+        public static final int etTip=0x7f060005;
+        public static final int tvTip=0x7f060000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/tipcalculator/MainActivity.java
+++ src/com/example/tipcalculator/MainActivity.java
@@ -6,41 +6,96 @@ import java.math.RoundingMode;
 
 import android.app.Activity;
 import android.os.Bundle;
-//import android.util.Log;
-//import android.view.Menu;
-//import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
+
 
 public class MainActivity extends Activity {
 	private EditText etAmount;
-	private TextView tvTip;
+	private EditText etTip;
+	private TextView tvTip;	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		etAmount = (EditText)findViewById(R.id.etAmount);
-		tvTip = (TextView)findViewById(R.id.tvTip);		
+		etTip = (EditText)findViewById(R.id.etTip);
+		tvTip = (TextView)findViewById(R.id.tvTip);
+
+		etAmount.addTextChangedListener(new TextWatcher() {
+		    @Override
+		    public void onTextChanged(CharSequence s, int start, int before, int count) {
+		        // Fires right as the text is being changed (even supplies the range of text)
+		    	BigDecimal tip = calculateTip();		    	 
+				tvTip.setText("$" + tip.toString());
+		    }
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		etTip.addTextChangedListener(new TextWatcher() {
+		    @Override
+		    public void onTextChanged(CharSequence s, int start, int before, int count) {
+		        // Fires right as the text is being changed (even supplies the range of text)
+		    	try{
+		    		BigDecimal tip = calculateTip();		    	 
+		    		tvTip.setText("$" + tip.toString());
+		    	}catch (Exception e){
+		    		// catch 
+		    	}
+		    }
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 	
-	protected BigDecimal calculateTip(Integer percent){		 
-		Double amount = Double.parseDouble(etAmount.getText().toString());
-		BigDecimal tip = BigDecimal.valueOf((double)(amount * percent / 100));
+	protected BigDecimal calculateTip(){
+		Integer percent = Integer.parseInt(etTip.getText().toString());
+		BigDecimal tip = new BigDecimal(0);
+		try{
+			Double amount = Double.parseDouble(etAmount.getText().toString());
+			tip = BigDecimal.valueOf((double)(amount * percent / 100));
+		}catch (Exception e){
+			// do something
+		}
 		return tip.setScale(2, RoundingMode.HALF_UP);
 	}
 	
 	public void tip10(View v){
-		BigDecimal tip = calculateTip(10);
-		tvTip.setText("$" + tip.toString());
+		etTip.setText("10");
 	}
 	public void tip15(View v){
-		BigDecimal tip = calculateTip(15);
-		tvTip.setText("$" + tip.toString());
+		etTip.setText("15");
 	}
 	public void tip20(View v){
-		BigDecimal tip = calculateTip(20);
-		tvTip.setText("$" + tip.toString());
+		etTip.setText("20");
 	}
+	
+	
 }
